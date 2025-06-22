810ef08d53ecf0e801d9c700926b69e6253ed897
melaryvmg <melaryvalenmg@gmail.com>
2025-06-22 14:39:35 -0400

    Archivos de base de datos

--- bd/cursos.js
+++ bd/cursos.js
@@ -1,1 +1,10 @@
+let curso = {//estructura de datos
+    id: 0,
+    titulo: "titulo",
+    horas: 0
+}
+let cursos = [curso] //arreglo
 
+module.exports = {
+    cursos
+}
---
+++ bd/estudiantes.js
@@ -0,0 +1,10 @@
+
+let estudiante = {//estructura de datos
+    id:0,
+    nombre: "nombre",
+    apellido: "apellido",
+    sexo: "sexo"
+}
+let estudiantes = [estudiante] //arreglo
+
+module.exports = { estudiantes };
